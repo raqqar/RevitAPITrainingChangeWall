120eef73ba1b1607c7f24eedd5b09cf6f3d22f07
Viacheslav Alekseev <VA_Alekseev@outlook.com>
2022-03-13 23:11:21 +0300

    Добавьте файлы проекта.

---
+++ Models/WallUtils.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingChangeWall
+{
+    public class WallUtils
+    {
+        public static List<Element> GetWallTypes(ExternalCommandData commandData, List<Element> element)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            
+            List<Element> elementList = element[0].GetValidTypes().Select(selectedObject => doc.GetElement(selectedObject)).ToList();
+            return elementList;
+        }
+    }
+}
---
+++ ViewModels/MainViewViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingChangeWall
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SaveCommand { get; }
+
+        public List<Element> PickedObjects { get; }
+
+        public List<Element> WallTypes { get; } 
+
+        public Element SelectedWallTypes { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            PickedObjects = SelectionUtils.PickObjects(commandData);
+            WallTypes = WallUtils.GetWallTypes(commandData, PickedObjects);
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (PickedObjects.Count == 0 || SelectedWallTypes == null)
+                return;
+
+            using (var ts = new Transaction(doc, "Set system type"))
+            {
+                ts.Start();
+                foreach (var pickedObject in PickedObjects)
+                {
+                    if(pickedObject is Wall)
+                    {
+                        var oWall = pickedObject as Wall;
+                        WallType wallType = SelectedWallTypes as WallType;
+                        oWall.WallType = wallType;
+                    }
+                }
+                ts.Commit();
+            }
+            RaiseCloseRequest();
+        }
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
